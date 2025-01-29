588b487938adcf49623042185edb8eb42c924c7c
IIIWinterIII <nikitavakulin39@gmail.com>
2025-01-29 19:44:45 +0300

    setup and assembly

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Form energy data',
+        short_name: 'Form energy data App',
+        description: 'Форма заполнения данных',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === 'document',
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'html-cache'
+            }
+          },
+          {
+            urlPattern: ({ request }) => request.destination === 'image',
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'image-cache'
+            }
+          }
+        ]
+      }
+    })
+  ]
+});
