7e973e6f2ae91ca38c77f264494159d379038c86
IIIWinterIII <nikitavakulin39@gmail.com>
2025-01-29 20:02:52 +0300

    creating a structure

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import "./Form.scss"
+import energyDrinks from "../energyDrinks.js";
+
+
+function Form() {
+    return (
+        <div className="container-form">
+            <h1>Добавление данных</h1>
+
+        </div>
+    );
+}
+
+export default Form;
---
+++ src/energyDrinks.js
@@ -0,0 +1,16 @@
+const energyDrinks = [
+    {
+      "id": 1,
+      "name": "Monster Energy",
+      "brand": "Monster",
+      "flavor": "Оригинальный",
+      "volume": 500,
+      "caffeine": 160,
+      "sugar": 54,
+      "description": "Классический энергетик с богатым вкусом и бодрящим эффектом.",
+      "image": "/images/monster-original.png"
+    },
+    
+  ]
+
+  export default energyDrinks
