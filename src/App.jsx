cff4fd81e5df65ff77247af505034b07c542d962
IIIWinterIII <nikitavakulin39@gmail.com>
2025-01-31 14:35:33 +0300

    add react dom and All pages for display all data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import "./App.css"
-import Form from "./components/Form";
+import { Link, Route, Routes } from "react-router-dom";
+
+import "./App.css";
+
+import Form from "./components/Form.jsx";
+import All from "./components/All.jsx";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div>
-      <Form />
+      
+      <main>
+        <Routes>
+          <Route path="/" element={<Form />} />
+          <Route path="/all" element={<All />} />
+        </Routes>
+      </main>
+      <nav>
+        <Link to="/">
+          <FontAwesomeIcon icon={faPlus} />
+        </Link>
+        <Link to="/all">
+          <FontAwesomeIcon icon={faBook} />
+        </Link>
+      </nav>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { registerSW } from 'virtual:pwa-register';
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
+import "./index.css";
+import App from "./App.jsx";
+import { HashRouter as Router } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>
+);
 
 registerSW();
