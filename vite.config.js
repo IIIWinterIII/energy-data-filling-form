2b4196d4be7824a608f84049994a35a57b5005ff
IIIWinterIII <nikitavakulin39@gmail.com>
2025-01-31 19:55:54 +0300

    коррекция после деплоя и добавления иконок для pwa

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,49 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/energy-data-filling-form/",
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       manifest: {
-        name: 'Form energy data',
-        short_name: 'Form energy data App',
-        description: 'Форма заполнения данных',
-        theme_color: '#000000',
+        name: "Form energy data",
+        short_name: "Form energy data App",
+        description: "Форма заполнения данных",
+        theme_color: "#000000",
         icons: [
           {
-            src: '',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "public/192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: '',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
+            src: "public/512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
       workbox: {
         runtimeCaching: [
           {
-            urlPattern: ({ request }) => request.destination === 'document',
-            handler: 'NetworkFirst',
+            urlPattern: ({ request }) => request.destination === "document",
+            handler: "NetworkFirst",
             options: {
-              cacheName: 'html-cache'
-            }
+              cacheName: "html-cache",
+            },
           },
           {
-            urlPattern: ({ request }) => request.destination === 'image',
-            handler: 'CacheFirst',
+            urlPattern: ({ request }) => request.destination === "image",
+            handler: "CacheFirst",
             options: {
-              cacheName: 'image-cache'
-            }
-          }
-        ]
-      }
-    })
-  ]
+              cacheName: "image-cache",
+            },
+          },
+        ],
+      },
+    }),
+  ],
 });
